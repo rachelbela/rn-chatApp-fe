d3473f8eacf793ad8819f1c964e1bc0e701a6a7a
rachelbela <rachel_life@126.com>
2025-08-14 18:49:45 +0800

    feat: markdown,markdown dark mode

--- components/chat/AIMessage.tsx
+++ components/chat/AIMessage.tsx
@@ -2,9 +2,13 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 import { ChatMessage } from '@/types/chat';
 import { HapticsSoft } from '@/utils/Haptics';
 import { Feather, Ionicons, MaterialIcons } from '@expo/vector-icons';
+import * as WebBrowser from 'expo-web-browser';
 import React, { useState } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
+import CodeHighlighter from 'react-native-code-highlighter';
+import Markdown from 'react-native-markdown-display';
 import { RFValue } from "react-native-responsive-fontsize";
+import { atomOneDark, atomOneLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { ThemedText } from '../ThemedText';
 
 interface Props {
@@ -13,6 +17,131 @@ interface Props {
 export default function AIMessage({ item }: Props) {
     const colorScheme = useColorScheme()
     const [show, setShow] = useState(true);
+    const onLinkPress = (url: string) => {
+        if (url) {
+            HapticsSoft();
+            console.log("onlinkPress")
+            Alert.alert("提示", "是否打开链接？", [{ text: "取消", style: "cancel" }, {
+                text: "确定", style: "default", onPress: async () => {
+                    await WebBrowser.openBrowserAsync('https://expo.dev');
+                }
+            }])
+            // some custom logic
+            return false;
+        }
+
+        // return true to open with `Linking.openURL
+        // return false to handle it yourself
+        return true
+    }
+    const rules = {
+        // @ts-ignore
+        link: (node, children) => {
+            return (
+                <Text key={node.key} style={{ color: "#007bff" }} onPress={() => onLinkPress(node.attributes.href)}>
+                    {children}
+                </Text>
+            );
+        },
+        // @ts-ignore
+        table: (node, children, parent, styles) => (
+            <View key={node.key} style={[styles._VIEW_SAFE_table, { borderColor: colorScheme === "dark" ? "white" : "black", borderRadius: 8 }]}>
+                {children}
+            </View>
+        ),
+        // @ts-ignore
+        tr: (node, children, parent, styles) => (
+            <View key={node.key} style={[styles._VIEW_SAFE_tr, { borderColor: colorScheme === "dark" ? "white" : "black" }]}>
+                {children}
+            </View>
+        ),
+        // @ts-ignore
+        textgroup: (node, children, parent, styles) => (
+            <ThemedText key={node.key} style={styles.textgroup}>
+                {children}
+            </ThemedText>
+        ),
+        // @ts-ignore
+        blockquote: (node, children, parent, styles) => (
+            <View key={node.key} style={[styles._VIEW_SAFE_blockquote, {
+                backgroundColor: colorScheme === "dark" ? "#292C33" : "#FAFAFA",
+                borderTopRightRadius: 8,
+                borderBottomRightRadius: 8
+            }]}>
+                {children}
+            </View>
+        ),
+        // Code
+        // @ts-ignore
+        code_inline: (node, children, parent, styles, inheritedStyles = {}) => (
+            <ThemedText key={node.key} style={{ fontWeight: "bold" }}>
+                {node.content}
+            </ThemedText>
+        ),
+        // @ts-ignore
+        code_block: (node, children, parent, styles, inheritedStyles = {}) => {
+            // we trim new lines off the end of code blocks because the parser sends an extra one.
+            let { content } = node;
+
+            if (
+                typeof node.content === 'string' &&
+                node.content.charAt(node.content.length - 1) === '\n'
+            ) {
+                content = node.content.substring(0, node.content.length - 1);
+            }
+
+            return (
+                <CodeHighlighter
+                    hljsStyle={colorScheme === "dark" ? atomOneDark : atomOneLight}
+                    containerStyle={{
+                        width: "100%",
+
+                    }}
+                    customStyle={{
+                        padding: 8,
+                        borderRadius: 8,
+                        backgroundColor: colorScheme === "dark" ? "#292C33" : "#FAFAFA"
+
+                    }}
+                    language={node.sourceInfo}
+                >
+                    {content}
+                </CodeHighlighter>
+            );
+        },
+        // @ts-ignore
+        fence: (node, children, parent, styles, inheritedStyles = {}) => {
+            // we trim new lines off the end of code blocks because the parser sends an extra one.
+            let { content } = node;
+
+            if (
+                typeof node.content === 'string' &&
+                node.content.charAt(node.content.length - 1) === '\n'
+            ) {
+                content = node.content.substring(0, node.content.length - 1);
+            }
+            console.log("node", node)
+            return (
+                <CodeHighlighter
+                    hljsStyle={colorScheme === "dark" ? atomOneDark : atomOneLight}
+                    containerStyle={{
+                        width: "100%",
+
+                    }}
+                    customStyle={{
+                        padding: 8,
+                        borderRadius: 8,
+                        backgroundColor: colorScheme === "dark" ? "#292C33" : "#FAFAFA"
+
+                    }}
+                    language={node.sourceInfo}
+                >
+                    {content}
+                </CodeHighlighter>
+            );
+        },
+    };
+
     return (
         <View style={[styles.container, { backgroundColor: colorScheme === "dark" ? "#0f0f0f" : "#ebebeb" }]}>
             {
@@ -134,7 +263,11 @@ export default function AIMessage({ item }: Props) {
                     </View>
                 </View>
             }
-            <ThemedText type="defaultSemiBold">{item.content}</ThemedText>
+            <ThemedText type="defaultSemiBold">
+                <Markdown rules={rules} debugPrintTree>
+                    {item.content}
+                </Markdown>
+            </ThemedText>
         </View>
     )
 }
